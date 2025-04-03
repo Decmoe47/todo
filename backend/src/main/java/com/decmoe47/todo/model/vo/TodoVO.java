dcc7d1ccb9caa55050e6156c80477335a0ba1e4c
Decmoe47 <lingsq47@outlook.com>
2025-04-03 11:30:50 +0900

    fix: 返回给前端的dueDate格式和接受的不一致

--- backend/src/main/java/com/decmoe47/todo/model/vo/TodoVO.java
+++ backend/src/main/java/com/decmoe47/todo/model/vo/TodoVO.java
@@ -1,5 +1,6 @@
 package com.decmoe47.todo.model.vo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -9,7 +10,10 @@ public class TodoVO {
 
     private long id;
     private String content;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     private LocalDateTime dueDate;
+
     private boolean done;
     private String description;
     private TodoListVO belongedList;
